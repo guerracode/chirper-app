45442fd16a51cdecd7a2beb6f8d7f5bc14ad4da2
Luis Chavez <josephsiul15@gmail.com>
2021-07-03 13:00:02 -0500

    adding tweets

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
+import Dashboard from './Dashboard';
+
+class App extends Component {
+   componentDidMount() {
+      this.props.dispatch(handleInitialData());
+   }
+
+   render() {
+      return (
+         <div>
+            {this.props.loading === true ? <h3>Loading...</h3> : <Dashboard />}
+         </div>
+      );
+   }
+}
+
+function mapStateToProps({ authedUser }) {
+   return {
+      loading: authedUser === null,
+   };
+}
+
+export default connect(mapStateToProps)(App);
