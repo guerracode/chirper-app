5365ca99f7d0ddea07724b14d1518dce7da8bb0e
Luis Chavez <josephsiul15@gmail.com>
2021-07-06 10:03:47 -0500

    project finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import LoadingBar from 'react-redux-loading';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
+import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
+import Nav from './Nav';
 
 class App extends Component {
    componentDidMount() {
@@ -10,9 +15,21 @@ class App extends Component {
 
    render() {
       return (
-         <div>
-            {this.props.loading === true ? <h3>Loading...</h3> : <Dashboard />}
-         </div>
+         <Router>
+            <Fragment>
+               <LoadingBar />
+               <div className="container">
+                  <Nav />
+                  {this.props.loading === true ? null : (
+                     <div>
+                        <Route path="/" exact component={Dashboard} />
+                        <Route path="/tweet/:id" component={TweetPage} />
+                        <Route path="/new" component={NewTweet} />
+                     </div>
+                  )}
+               </div>
+            </Fragment>
+         </Router>
       );
    }
 }
